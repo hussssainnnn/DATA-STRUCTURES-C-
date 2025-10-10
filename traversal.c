b0eb585397b46aace6471d1396e80be942e6c7c4
hussssainnnn <muhammadhussainx7@gmail.com>
2025-10-10 10:52:02 +0530

    Add files via upload

---
+++ traversal.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a[50], size, i;
+
+    printf("Enter size of array: ");
+    scanf("%d", &size);
+
+    printf("Enter elements of array:\n");
+    for (i = 0; i < size; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Elements in array are:\n");
+    for (i = 0; i < size; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
